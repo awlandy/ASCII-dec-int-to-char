5a727c94b5597786c196408f985ef2aca7106d5b
Andy <40180962+awlandy@users.noreply.github.com>
2019-04-16 20:22:33 +0800

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -1,13 +1,14 @@
-#include "pch.h"
+//#include "pch.h" I personally use Visual Studio 2017
 #include <iostream>
 
+//my notes for the ASCII integar decimal values
 //DEC 48-57 0-9
 //DEC 65-90 A-Z
 //DEC 97-122 a-z
 
 void print(int b, int e){
 	for (int x = b; x <= e; ++x) {
-	std::cout << (char)x;
+	std::cout << (char)x; //explixitly converts int to char
 	}
 	std::cout << "\n";
 }
